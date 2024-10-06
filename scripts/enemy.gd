61581cbad6b9aed22a5fc1140981d6fa4956ebe2
gustavo-tomas <guga.tomas10@gmail.com>
2024-10-06 11:48:22 -0300

    Adding enemy animated sprite

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,8 +2,11 @@ extends RigidBody3D
 
 var hitpoints: float = 100
 
+@onready var animation = $AnimatedSprite3D;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    animation.play("walk")
     return
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
